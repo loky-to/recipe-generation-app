c91d27f2cc877f0c9beb25b666119610c44f2786
Loky <lokyto18@gmail.com>
2025-05-30 19:25:16 +1000

    DEV | updating for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    images: { unoptimized: true },
+    basePath: '/recipe-generation-app',
+    assetPrefix: '/recipe-generation-app/',
+  };
 
 export default nextConfig;
